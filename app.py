477a8f8f06d9d3cc27a0aee61d164b8ac8fbe23a
daniyalk26 <150397501+daniyalk26@users.noreply.github.com>
2025-04-11 17:29:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def main():
     """)
 
     # Grab query params to see if we have ?code=<XYZ>
-    query_params = st.query_params
+    query_params = st.experimental_get_query_params()
 
     
 
